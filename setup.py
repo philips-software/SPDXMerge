7db043869f934a16bd6b890209074f4784385699
anniejovitha.arulanandam <anniejovitha.arulanandam@philips.com>
2023-04-06 10:12:18 +0200

    test change permissions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(requirements_path, "r", encoding="utf8") as f:
 
 setup(
     name='spdxmerge',
-    version='0.1.7',
+    version='0.1.8',
     description="merges content of two/more spdx sboms",
     long_description_content_type="text/markdown",
     url="https://github.com/philips-software/SPDXMerge",
