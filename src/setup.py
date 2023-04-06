30890ad7a11f81cf8d2630d66f40f109c0683f60
Jeroen Knoops <jeroen.knoops@philips.com>
2023-04-06 09:30:35 +0100

    moved setup to src folder
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
-requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+requirements_path = os.path.join(os.path.dirname(__file__), '../requirements.txt')
 
 with open(requirements_path, "r", encoding="utf8") as f:
     required = f.read().splitlines()
@@ -13,7 +13,7 @@ setup(
     description="merges content of two/more spdx sboms",
     long_description_content_type="text/markdown",
     url="https://github.com/philips-software/SPDXMerge",
-    packages=find_packages(include=['src'], exclude=['test', '*.test', '*.test.*']),
+    packages=find_packages(include=['.'], exclude=['test', '*.test', '*.test.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -23,6 +23,6 @@ setup(
     python_requires='>=3.10.9',
     entry_points='''
         [console_scripts]
-        spdxmerge=src.SPDXMerge:main
+        spdxmerge=SPDXMerge:main
     '''
 )
